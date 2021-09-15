a5530aa8aa573fe43273d969a36a3a4346cc28a1
Jawad63 <jawadqureshi111@gmail.com>
2021-09-15 14:31:21 +0200

    empty functions created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+
 const App = () => {
+
+
+  // we will make the API call with this: 
+  const startTrivia = async () => {
+
+  }
+
+  // this function will be triggered when a user selects an answer: 
+  // We are specifiying the event by telling it it'll come from HTML 
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  // this function will be triggered when the user clicks for the next Question:
+  const nextQuestion = () => {
+
+  }
+
+
   return (
     <div className="App">Quiz App:</div>
   );
+  
 }
 
 export default App;
